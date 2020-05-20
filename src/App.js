e8af23101344c6057172b53419566cc756e1cc90
Nik Rowe <NikRowe7@Gmail.com>
2020-05-20 11:44:35 -0700

    Let there be code!

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>3C Home Page</h1>
+      <h2>You got this Nik!</h2>
+    </div>
+  );
+}
