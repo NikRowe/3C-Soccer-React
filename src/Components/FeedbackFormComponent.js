67507589cb5bf9b685ceea33be27e4a674906675
Nik Rowe <NikRowe7@Gmail.com>
2020-05-26 01:03:31 -0700

    Form Email Submission

--- src/Components/FeedbackFormComponent.js
+++ src/Components/FeedbackFormComponent.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form, FormGroup, Input, Label, Col, Row } from 'reactstrap'
 
 function FeedbackForm() {
+    
+    const [ playerName, setPlayerName ] = useState('')
+    const [ parentName, setParentName ] = useState('')
+    const [ DOB, setDOB ] = useState('')
+    const [ address, setAddress ] = useState('')
+    const [ phone, setPhone ] = useState('')
+    const [ yearsPlayed, setYearsPlayed ] = useState('')
+
+    const submit = e => {
+        e.preventDefault();
+        fetch(`https://hooks.zapier.com/hooks/catch/7634421/oiqrim1/`, {
+          method: 'POST',
+          body: JSON.stringify({ playerName, parentName, DOB, address, phone, yearsPlayed }),
+        })
+      }
+    
     return (
-        <Form>
+        <Form onSubmit={values => submit(values)}>
             <FormGroup>
                 <Label htmlFor='playerName'>Player Name</Label>
                 <Input
@@ -11,6 +27,8 @@ function FeedbackForm() {
                     type='text'
                     id='playerName'
                     name='playerName'
+                    value={playerName}
+                    onChange={e => setPlayerName(e.target.value)}
                 />
             </FormGroup>
             <FormGroup>
@@ -20,6 +38,8 @@ function FeedbackForm() {
                     type='text'
                     id='parentName'
                     name='parentName'
+                    value={parentName}
+                    onChange={e => setParentName(e.target.value)}
                 />
             </FormGroup>
             <FormGroup>
@@ -29,6 +49,8 @@ function FeedbackForm() {
                     type='date'
                     id='DOB'
                     name='DOB'
+                    value={DOB}
+                    onChange={e => setDOB(e.target.value)}
                 />
             </FormGroup>
             <FormGroup>
@@ -38,6 +60,8 @@ function FeedbackForm() {
                     id='address'
                     name='address'
                     placeholder=' (optional) '
+                    value={address}
+                    onChange={e => setAddress(e.target.value)}
                 />
             </FormGroup>
             <FormGroup>
@@ -50,6 +74,8 @@ function FeedbackForm() {
                     maxLength='12'
                     minLength='12'
                     placeholder='xxx-xxx-xxxx'
+                    value={phone}
+                    onChange={e => setPhone(e.target.value)}
                 />
             </FormGroup>
             <FormGroup>
@@ -61,9 +87,11 @@ function FeedbackForm() {
                     name='yearsPlayed'
                     maxLength='2'
                     minLength='1'
+                    value={yearsPlayed}
+                    onChange={e => setYearsPlayed(e.target.value)}
                 />
             </FormGroup>
-            <Col sm={{ offset: 4}}>
+            <Col sm={{ offset: 4 }}>
                 <Button type='submit' className='regButton'>Register</Button>
             </Col>
         </Form>
